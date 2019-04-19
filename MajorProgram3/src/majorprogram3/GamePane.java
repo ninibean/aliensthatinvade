1400f0b0c1b813f91c6406a47a3be0135c380a92
Niara Patterson <nspatterson@aggies.ncat.edu>
2019-04-18 22:55:25 -0400

    Level Two completed

--- MajorProgram3/src/majorprogram3/GamePane.java
+++ MajorProgram3/src/majorprogram3/GamePane.java
@@ -5,6 +5,7 @@
  */
 package majorprogram3;
 
+import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
@@ -19,6 +20,7 @@ public class GamePane extends BorderPane {
     
     public GamePane() {
         actionPane = new ActionPane();
+        MyTimer timer = new MyTimer();
        // this.setCenter(actionPane);
         
         cmdCenter = new CmdCenter(actionPane);
@@ -26,6 +28,10 @@ public class GamePane extends BorderPane {
         MyCmdHandler mch = new MyCmdHandler();
         this.setOnKeyPressed(mch);
         this.getChildren().add(actionPane);
+        
+        timer.start();
+        
+        
     }
     
     
@@ -51,10 +57,23 @@ public class GamePane extends BorderPane {
                     case SPACE:
                         //System.out.println("Space key pressed");
                         cmdCenter.fireProjectile();
-                        System.out.println("S");
+                        //cmdCenter.projectile.setSpeed(-15);
+                        //cmdCenter.projectile.setDirection(90);
+                         System.out.println("S");
                         break;
                     default:
                         break;
+            }
+        }
+    }
+    
+    public class MyTimer extends AnimationTimer {
+       private long previous = 0;
+        //@Override
+        public void handle(long now) {
+            if (now - previous >= 2500000L ) { //L makes a large number passable as an int value
+                cmdCenter.projectile.move();
+                previous = now;
             }
         }
     }
